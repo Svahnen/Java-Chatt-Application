1d3898b5946cee936657256aa40865681a8ecdfb
Svahnen <svahn.jonny@gmail.com>
2022-04-18 09:52:38 +0200

    Cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Receiver started");
-        // MulticastSender sender = new MulticastSender();
         Multicast multicast = new Multicast();
         Listen listen = new Listen(multicast);
-        GUI giu = new GUI(multicast);
         listen.start();
+        GUI giu = new GUI(multicast);
 
     }
 }
